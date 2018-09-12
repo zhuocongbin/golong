a89e5d55e8f2890a5c22b52253b0ad8d9d23f7fc
zkbqhuang <1179972750qq.com>
2018-09-12 14:22:20 +0800

    添加mian.go中的注释

--- project/gopush/comet/main.go
+++ project/gopush/comet/main.go
@@ -12,22 +12,22 @@ import (
 )
 
 func main() {
-	// parse cmd-line arguments
+	// parse cmd-line arguments  //解析cmd-line参数
 	flag.Parse()
 	log.Info("comet ver: \"%s\" start", ver.Version)
-	// init config
+	// init config //初始化配置
 	if err := InitConfig(); err != nil {
 		panic(err)
 	}
-	// set max routine
+	// set max routine //设置最大线程
 	runtime.GOMAXPROCS(Conf.MaxProc)
-	// init log
+	// init log //初始化日志
 	log.LoadConfiguration(Conf.Log)
 	defer log.Close()
 	// start pprof
 	perf.Init(Conf.PprofBind)
-	// create channel
-	// if process exit, close channel
+	// create channel //创建通道
+	// if process exit, close channel //如果进程存在，关闭通道
 	UserChannel = NewChannelList()
 	defer UserChannel.Close()
 	// start stats
