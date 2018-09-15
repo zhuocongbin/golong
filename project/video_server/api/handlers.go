010aeda51b9c10b8f012556e6c6e99d9eceff854
zkbqhuang <1179972750qq.com>
2018-09-15 10:14:43 +0800

    更新 handlers

--- project/video_server/api/handlers.go
+++ project/video_server/api/handlers.go
@@ -6,6 +6,11 @@ import(
      "github.com/julienschmidt/httprouter"
 )
 
-func CreateUser() {
+func CreateUser(w,http.ResponseWriter,r *http.Request,p httprouter.Params) {
 	io.WriteString(w,"Create User Handler")
+}
+
+func Login(w,http.ResponseWriter,r *http.Request,p httprouter.Params) {
+	uname := p.ByName("user_name")
+	io.WriteString(w, uname)
 }
