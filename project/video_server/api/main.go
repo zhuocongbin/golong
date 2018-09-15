d085653de902a7259684b5e724505575c360d911
zkbqhuang <1179972750qq.com>
2018-09-15 10:31:18 +0800

    更新 main 和handlers

--- project/video_server/api/main.go
+++ project/video_server/api/main.go
@@ -1,24 +1,49 @@
-package main 
+package main
 
-//一个性能良好的HTTP请求路由器 julienschmidt/httprouter
-import(
-     
-     "net/http" 
-     "github.com/julienschmidt/httprouter"
-
+import (
+	"net/http"
+	"github.com/julienschmidt/httprouter"
+	"log"
+	"video_server/api/session"
 )
 
-func RegisterHandlers() *httprouter.Router {
-	router := httprouter.New()
-	//创建用户
-	router.POST("/user",CreateUser)
-	//用户登录
-	router.POST("/user/:user_name", Login)
-	
-	return router
-	
+type middleWareHandler struct {
+	r *httprouter.Router
 }
-func func main() {
-	r := RegisterHandlers()
-	http.ListenAndServe(":8000",r)
+
+func NewMiddleWareHandler(r *httprouter.Router) http.Handler {
+	m:=middleWareHandler{}
+	m.r = r
+	return m
+}
+
+func (m middleWareHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	//check session
+	validateUserSession(r)
+	m.r.ServeHTTP(w, r)
+}
+
+func RegisterHandlers() *httprouter.Router {
+	log.Printf("preparing to post request\n")
+	router:=httprouter.New()
+	router.POST("/user", CreateUser)
+	router.POST("/user/:username", Login)
+	router.GET("/user/:username", GetUserInfo)
+	router.POST("/user/:username/videos", AddNewVideo)
+	router.GET("/user/:username/videos", ListAllVideos)
+	router.DELETE("/user/:username/videos/:vid-id", DeleteVideo)
+	router.POST("/videos/:vid-id/comments", PostComment)
+	router.GET("/videos/:vid-id/comments", ShowComments)
+	return router
+}
+
+func Prepare() {
+	session.LoadSessionsFromDB()
+}
+
+func main() {
+	Prepare()
+	r:=RegisterHandlers()
+	mh:=NewMiddleWareHandler(r)
+	http.ListenAndServe(":8000", mh)
 }
